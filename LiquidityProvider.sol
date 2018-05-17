abf75babcb8666e4c00fa89d6e9e0e46d9fcee39
Pavel Khaskel <haskel.pavel@gmail.com>
2018-05-17 19:36:44 +0300

    init

---
+++ LiquidityProvider.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.0;
+
+contract LiquidityProvider {
+
+    address private $tokenContract = 0x123123;
+
+    int32 $depositMinSum = 0;
+    int32 $depositMaxSum = 0;
+    int32 $depositMinPeriod = 0;
+    int32 $depositMaxPeriod = 0;
+
+    // расрешеные биржи для торгов
+    address[] $allowedContractors;
+
+    function LiquidityProvider() {
+
+    }
+
+    function deposit() {
+
+    }
+
+    function claim() {
+
+    }
+
+    function order() {
+
+    }
+
+    function pauseDeposit(isPaused) {
+
+    }
+
+
+
+
+
+}
---
+++ ProviderAggregator.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.0;
+
+contract ProviderAggregator {
+    function ProviderAggregator() {
+
+    }
+
+    function addProvider() {
+
+    }
+
+    function removeProvider() {
+
+    }
+
+    function pauseProvider() {
+
+    }
+
+    function getMarketVolume() {
+
+    }
+
+}
